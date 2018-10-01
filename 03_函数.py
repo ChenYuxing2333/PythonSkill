9a3412ff4c5d30e8d76d73b74fb0dab6510367d2
Jason <1259815656@qq.com>
2018-10-01 12:10:41 +0800

    01.学习内容 02.第一个Python程序 03.九九乘法表

--- 03_函数.py
+++ 03_函数.py
@@ -7,3 +7,34 @@ num_str = "0123456789"
 print(num_str[2:6])
 print(num_str[2:])
 print(num_str[:6])
+# 下一个
+students = [
+    {"name": "阿土",
+     "age": 20,
+     "gender": True,
+     "height": 1.7,
+     "weight": 75.0},
+    {"name": "小美",
+     "age": 19,
+     "gender": False,
+     "height": 1.6,
+     "weight": 45.0},
+]
+
+find_name = "阿"
+
+for stu_dict in students:
+
+    print(stu_dict)
+
+    # 判断当前遍历的字典中姓名是否为find_name
+    if stu_dict["name"] == find_name:
+        print("找到了")
+
+        # 如果已经找到，直接退出循环，就不需要再对后续的数据进行比较
+        break
+
+else:
+    print("没有找到")
+
+print("循环结束")
